76c510cb8d2eda4eb791e824a4ef401036d0a370
I4ze <Iaze.guilherme25@gmail.com>
2024-04-25 22:37:31 -0300

    Tela de exercicio adicionada

--- lib/Screens/exercicio_tela.dart
+++ lib/Screens/exercicio_tela.dart
@@ -1,34 +1,54 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 import "package:flutter_gymapp/Models/exercicio_model.dart";
+import "package:flutter_gymapp/Models/sentimento_model.dart";
 
 class ExercicioTela extends StatelessWidget {
   ExercicioTela({super.key});
 
   final Exercicio exercicioModelo = Exercicio(
-    "ex01",
-    "Remada baixa supinada",
-    "Treino A",
-    "Segura a barra e puxa"
-  );
+      "ex01", "Remada baixa supinada", "Treino A", "Segura a barra e puxa");
 
-  final List<SentimentoModel> listaSentimentos
+  final List<SentimentoModel> listaSentimentos = [
+    SentimentoModel("SE001", "pouca ativação hoje", "25/04/2024"),
+    SentimentoModel("SE002", "senti ativação hoje", "27/04/2024"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: const Text("Puxada alta pronada - Treino A"),
+          title: Text("${exercicioModelo.name} - ${exercicioModelo.treino}"),
           backgroundColor: Colors.blue,
         ),
-        body: Padding(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("button add was pressed");
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Container(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          margin: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(16)),
+          child: ListView(
             children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Enviar foto"),
+              SizedBox(
+                height: 250,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Enviar foto"),
+                    ),
+                    ElevatedButton(onPressed: (){}, child: const Text("Remover Foto"))
+                  ],
+                ),
               ),
+              const SizedBox(height: 8),
               const Text(
                 "Como fazer?",
                 style: TextStyle(
@@ -36,9 +56,12 @@ class ExercicioTela extends StatelessWidget {
                   fontSize: 18,
                 ),
               ),
-              const Text(
-                  "Segura com as duas mão na barra, mantém a coluna reta e puxa"),
-              const Divider(),
+              const SizedBox(height: 8),
+              Text(exercicioModelo.comoFazer),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Divider(color: Colors.black),
+              ),
               const Text(
                 "Como estou me sentindo?",
                 style: TextStyle(
@@ -46,7 +69,19 @@ class ExercicioTela extends StatelessWidget {
                   fontSize: 18,
                 ),
               ),
-              const Text("Senti bastante ativação hoje!")
+              const SizedBox(height: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(listaSentimentos.length, (index) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    title: Text(listaSentimentos[index].sentindo), 
+                    subtitle: Text(listaSentimentos[index].data),
+                    leading: const Icon(Icons.double_arrow),
+                    trailing: IconButton(icon: const Icon(Icons.delete, color: Colors.red), onPressed: (){},),
+                  );
+                }),
+              )
             ],
           ),
         ));
